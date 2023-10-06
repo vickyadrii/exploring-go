1e784da929904e1a05cef8d8b690bbd1fcca1592
vickyadri29 <vickyadri290501@gmail.com>
2023-10-06 21:09:50 +0800

    add task 2

--- day5/main.go
+++ day5/main.go
@@ -14,10 +14,35 @@ func (car Car) predictDistance() float32 {
 	return distance
 }
 
+
+// Soal 2
+type Student struct {
+	name []string
+	score []int
+}
+
+
 func main() {
 	// Soal 1
 	var car Car
 	car.FullIn = 2
 	distance := car.predictDistance()
 	fmt.Println(distance)
+
+
+	// Soal 2
+	var students Student
+	for i := 0; i <= 4; i++ {
+		students.name = append(students.name, "")
+		students.score = append(students.score, 0)
+
+		fmt.Println("Masukkan nama murid ke ", i+1, " : ")
+		fmt.Scanln(&students.name[i])
+		
+		fmt.Println("Masukkan nilai murid ke ", i+1, " : ")
+		fmt.Scanln(&students.score[i])
+	}
+
+	fmt.Println(students)
+	
 }
